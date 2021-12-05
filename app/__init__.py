cb3add9064a6557ed8e439487aeb33c761c72d94
unlomtrois <vadim.kuz02@gmail.com>
2021-12-05 15:53:05 +0300

    delete useless code

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
 from app.user import router as user_router
 
-from .db import Base, engine
-
 app = FastAPI()
-
-Base.metadata.create_all(engine)
-
 
 app.include_router(user_router)
 # app.include_router(item_main.router)
