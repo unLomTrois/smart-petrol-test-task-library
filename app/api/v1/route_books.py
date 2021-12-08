a03d14c54dbe4ce92ac698ad007e3d9e03cf554f
unlomtrois <vadim.kuz02@gmail.com>
2021-12-08 06:09:07 +0300

    add role check on add_book post

--- app/api/v1/route_books.py
+++ app/api/v1/route_books.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.api.utils import RoleChecker
+from app.api.utils import check_role
 from app.db import Session, get_db
 
 from app.db import models
@@ -9,10 +9,8 @@ from app.schemas import books as book_schamas
 # APIRouter creates path operations for user module
 router = APIRouter(responses={404: {"description": "Not found"}}, )
 
-allow_create_resource = RoleChecker(["booker"])
 
-
-@router.get("/", dependencies=[Depends(allow_create_resource)])
+@router.get("/")
 async def read_root(db: Session = Depends(get_db)):
 
     books = get_books(db)
@@ -20,7 +18,7 @@ async def read_root(db: Session = Depends(get_db)):
     return books
 
 
-@router.post("/add")
+@router.post("/add", dependencies=[Depends(check_role(["librarian"]))])
 async def add_book(book: book_schamas.BookCreate,
                    db: Session = Depends(get_db)):
     return create_book(db, book)
