54143e3dc2065a7c948b00abcfaf4bd41254c00b
unlomtrois <vadim.kuz02@gmail.com>
2021-12-12 14:57:33 +0300

    add cors policy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import Base, engine
 from app.api import api_router
+
 
 def include_router(app: FastAPI):
     app.include_router(api_router)
@@ -10,6 +12,21 @@ def include_router(app: FastAPI):
 #     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def enable_cors(app: FastAPI):
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
@@ -17,6 +34,7 @@ def create_tables():
 def start_application():
     app = FastAPI()
     include_router(app)
+    enable_cors(app)
     # configure_static(app)
     create_tables()
     return app
